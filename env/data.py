0254d64d4e691ae993a96e972c89a2fb62a928a4
ronaleane <rosal.ronaleane@gmail.com>
2022-12-27 21:05:19 +0800

    fixed files

--- env/data.py
+++ env/data.py
@@ -3,19 +3,25 @@ menu = {
         {
             'name': 'Cheese',
             'availability' : True,
-            'price': 790,
+            'small_price': 800,
+            'medium_price': 850,
+            'large_price': 900,
             'url': 'https://cdn.pixabay.com/photo/2017/11/26/08/42/pizza-2978377_960_720.jpg'
         },
         {
             'name': 'Veggie',
             'availability' : False,
-            'price': 790,
+            'small_price': 800,
+            'medium_price': 850,
+            'large_price': 900,
             'url': 'https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg'
         },
         {
             'name': 'Pepperoni',
             'availability' : True,
-            'price': 790,
+            'small_price': 800,
+            'medium_price': 850,
+            'large_price': 900,
             'url': 'https://cdn.pixabay.com/photo/2016/03/05/21/45/pizza-1239077_960_720.jpg'
         }
     ],
@@ -23,13 +29,17 @@ menu = {
         {
             'name': 'Burger',
             'availability' : True,
-            'price': 50,
+            'small_price': 50,
+            'medium_price': 75,
+            'large_price': 100,
             'url': 'https://cdn.pixabay.com/photo/2017/11/12/21/11/hamburger-2943825_960_720.jpg'
         },
         {
             'name': 'Fries',
             'availability' : True,
-            'price': 45,
+            'small_price': 45,
+            'medium_price': 60,
+            'large_price': 75,
             'url': 'https://cdn.pixabay.com/photo/2016/04/25/01/58/french-fries-1351062_960_720.jpg'
         }
     ],
@@ -37,25 +47,33 @@ menu = {
         {
             'name': 'Coke',
             'availability' : True,
-            'price': 15,
+            'small_price': 15,
+            'medium_price': 20,
+            'large_price': 25,
             'url': 'https://cdn.pixabay.com/photo/2017/02/25/23/12/coca-cola-2099000_960_720.jpg'
         },
         {
             'name': 'Orange Juice',
             'availability' : False,
-            'price': 25,
+            'small_price': 25,
+            'medium_price': 30,
+            'large_price': 35,
             'url': 'https://cdn.pixabay.com/photo/2017/01/20/14/59/orange-1995044_960_720.jpg'
         },
         {
             'name': 'Water',
             'availability' : True,
-            'price': 10,
+            'small_price': 10,
+            'medium_price': 15,
+            'large_price': 20,
             'url': 'https://cdn.pixabay.com/photo/2017/10/21/16/07/glass-2875091_960_720.jpg'
         },
         {
             'name': 'Iced Tea',
             'availability' : False,
-            'price': 20,
+            'small_price': 20,
+            'medium_price': 25,
+            'large_price': 30,
             'url': 'https://cdn.pixabay.com/photo/2014/12/11/03/12/lemon-tea-563806_960_720.jpg'
         }
     ]
